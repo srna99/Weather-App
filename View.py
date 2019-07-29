2ed2a45dbc10b27a01d96948b1554ddb67a3b719
Serena Cheng <serena.ch1999@gmail.com>
2019-07-29 16:55:23 -0400

    Add ability to change city.

--- View.py
+++ View.py
@@ -14,21 +14,31 @@ class View:
         root.geometry("325x500")
         root.config(bg="gray")
 
-        self.loc_label = Label(font=("Arial", 20), fg="white", bg="gray")
-        self.weather_icon = Label(bg="gray")
-        self.temp_label = Label(font=("Arial", 70), fg="white", bg="gray")
+        entry_frame = Frame(root, height=40, padx=20, pady=10, bg="gray")
+        entry_frame.grid(row=1)
 
-        self.update_gui()
+        self.entry = Entry(entry_frame, font=("Arial", 12))
+        get_weather_button = Button(entry_frame, text="Search", font=("Arial", 9), bg="dark gray", command=lambda: self.update_weather(self.entry.get()))
 
-        self.loc_label.grid(row=1)
-        self.weather_icon.grid(row=2)
-        self.temp_label.grid(row=3)
+        self.entry.pack(padx=5, expand=TRUE, side=LEFT)
+        get_weather_button.pack(expand=TRUE, padx=5, side=LEFT)
+
+        self.loc_label = Label(root, font=("Arial", 20), fg="white", bg="gray")
+        self.weather_icon = Label(root, bg="gray")
+        self.temp_label = Label(root, font=("Arial", 70), fg="white", bg="gray")
+
+        self.update_weather("London")
+
+        self.loc_label.grid(row=2, column=0, columnspan=2)
+        self.weather_icon.grid(row=3, column=0, columnspan=2)
+        self.temp_label.grid(row=4, column=0, columnspan=2)
 
         root.rowconfigure(0, weight=1)
-        root.rowconfigure(1, weight=0)
+        root.rowconfigure(1, weight=1)
         root.rowconfigure(2, weight=0)
         root.rowconfigure(3, weight=0)
-        root.rowconfigure(4, weight=2)
+        root.rowconfigure(4, weight=0)
+        root.rowconfigure(5, weight=2)
         root.columnconfigure(0, weight=1)
 
     def update_weather(self, loc: str):
@@ -37,12 +47,15 @@ class View:
         self.img_path = self.owm.get_weather_icon()
         self.location = self.owm.get_location()
 
+        self.update_gui()
+
     def update_gui(self):
         self.loc_label.config(text=self.location)
         self.temp_label.config(text=self.temp)
 
         image = ImageTk.PhotoImage(Image.open(self.img_path))
         self.weather_icon.config(image=image)
+        self.weather_icon.image = image
 
 
 window = Tk()
